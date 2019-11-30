3bd09078baec9ffdaa0326d68f13b9037099e610
dark3691011 <37678562+dark3691011@users.noreply.github.com>
2019-11-30 08:22:18 +0700

    model NhanVien + BangChamCong

---
+++ Nhom15/Models/BangChamCong.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nhom15.Models
+{
+    public class BangChamCong
+    {
+        [Key]
+        [Display(Name="Mã bảng chấm công")]
+        public int MaBCC { get; set; }
+        [Display(Name ="Số ngày làm")]
+        public int SoNgayLam { get; set; }
+        [Display(Name ="Tháng")]
+        public int Thang { get; set; }
+        [Display(Name ="Lương")]
+        public string Luong { get; set; }
+
+
+        public int MaNV { get; set; }
+        [ForeignKey("MaNV")]
+        public NhanVien NhanVien { get; set; }
+    }
+}
---
+++ Nhom15/Models/NhanVien.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nhom15.Models
+{
+    public class NhanVien
+    {
+        [Key]
+        [Display(Name ="Mã nhân viên")]
+        public int MaNV { get; set; }
+        [Display(Name ="Tên nhân viên")]
+        public string TenNV { get; set; }
+        [Display(Name ="Lương")]
+        public string Luong { get; set; }
+
+        [Display(Name ="Mã phòng ban")]
+        public int MaPB { get; set; }
+        [ForeignKey("MaPB")]
+        public PhongBan PhongBan { get; set; } 
+    }
+}
